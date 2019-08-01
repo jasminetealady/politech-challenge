ad160cd2d94f2593fa2de865bd825789ec0d3d52
Razelle McCarrick <razelle@satyaorthopedic.com>
2019-08-01 15:41:45 -0400

    content for search section stubbed out w styling, added node-sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './css/App.css';
+import './css/App.scss';
 import Main from './components/Main.js';
 
 function App() {
--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,27 @@ class Favorites extends Component {
 	render() {
 		return (
 			<div className="Search">
-				<div className="Search-Input">Search Section</div>
+				<div className="Search-Wrapper">
+					<div className="Search-Text">
+						<p>
+							Find out how weird you are by selecting the GIFs that make you
+							laugh. We'll show you the least weird ones to start, but you can
+							move the slider to make them weirder.
+						</p>
+						<br />
+						<p>
+							When you find a GIF you like, press the <i>Like</i> button. Once
+							you like 5 GIFs, we'll show you how weird you are.
+						</p>
+					</div>
+					<div className="Search-Input">
+						<p>Search Term</p>
+						<form>
+							<input type="text" placeholder="Type your search term here ..." />
+							<button>Search</button>
+						</form>
+					</div>
+				</div>
 				<Results />
 			</div>
 		);
