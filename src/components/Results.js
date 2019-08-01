ed6364a42666729a8a17f35abe72b03f67216335
Razelle McCarrick <razelle@satyaorthopedic.com>
2019-08-01 16:22:24 -0400

    stubbed out results section w styling, slider, and state changes for slider

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 
 class Results extends Component {
+	state = {
+		weirdnessLevel: 0
+	};
+
+	handleChange = e => {
+		this.setState({ weirdnessLevel: e.target.value });
+	};
+
 	render() {
-		return <div className="Results">Results Here</div>;
+		return (
+			<div className="Results">
+				<h2>Your Result</h2>
+				<div className="GIF">
+					<p>GIF Name</p>
+					<p>Image here</p>
+					<button>Like Thumb</button>
+				</div>
+				<div className="Slider">
+					<input
+						type="range"
+						min="0"
+						max="10"
+						defaultValue="0"
+						onChange={this.handleChange}
+					/>
+					<p>Weirdness: {this.state.weirdnessLevel}</p>
+				</div>
+			</div>
+		);
 	}
 }
 
