d481cb98e3d10fe6f11661c17a5b9271290e6f78
Razelle McCarrick <razelle@satyaorthopedic.com>
2019-08-02 10:58:47 -0400

    added liking functionality in results component, error handling if max gifs selected

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,13 +3,30 @@ import GIF from './GIF.js';
 
 class Results extends Component {
 	state = {
-		weirdnessLevel: 0,
-		loadingWeirder: false
+		loadingWeirder: false,
+		likedGifs: [],
+		error: false
 	};
 
 	handleChange = e => {
 		this.setState({ weirdnessLevel: e.currentTarget.value });
 		this.props.fetchGif(e.currentTarget.value);
+	};
+
+	handleClick = () => {
+		let gifs = this.state.likedGifs;
+		let likedGif = {
+			name: this.props.gifName,
+			url: this.props.url,
+			weirdnessLevel: this.props.weirdness
+		};
+		if (gifs.length < 5) {
+			gifs.push(likedGif);
+		} else {
+			this.setState({ error: true });
+		}
+		console.log(gifs);
+		this.setState({ likedGifs: gifs });
 	};
 
 	render() {
@@ -22,19 +39,29 @@ class Results extends Component {
 					loading={this.props.loading}
 					loadingWeirder={this.state.loadingWeirder}
 				/>
-				<div className="Results-Button-Wrapper">
-					<button>Like Thumb</button>
-				</div>
-				<div className="Slider">
-					<input
-						type="range"
-						min="0"
-						max="10"
-						defaultValue="0"
-						onChange={this.handleChange}
-					/>
-					<p>Weirdness: {this.state.weirdnessLevel}</p>
-				</div>
+				{this.props.url && (
+					<div className="Results-Button-Wrapper">
+						<button onClick={this.handleClick}>Like</button>
+					</div>
+				)}
+				{this.props.url && (
+					<div className="Slider">
+						<input
+							type="range"
+							min="0"
+							max="10"
+							defaultValue="0"
+							onChange={this.handleChange}
+						/>
+						<p>Weirdness: {this.props.weirdness}</p>
+					</div>
+				)}
+				{this.state.error && (
+					<p class="Max-GIFs-Error">
+						You have selected the maximum number of GIFs. Please <i>unlike</i>{' '}
+						one of your GIFs if you would like to <i>like</i> another!
+					</p>
+				)}
 			</div>
 		);
 	}
