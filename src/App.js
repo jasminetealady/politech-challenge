f4df002e071fcbadbbf0b037e2a2ce8ed27e2cef
Razelle McCarrick <razelle@satyaorthopedic.com>
2019-08-02 12:22:34 -0400

    mapped state to props in App component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './css/App.scss';
 import Main from './components/Main.js';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
-function App() {
+function App(props) {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<h1>Weirdness Calculator</h1>
 			</header>
-			<Main />
+			<Main userData={this.props.userData} />
 		</div>
 	);
 }
 
-export default App;
+// export default App;
+
+const mapStateToProps = state => {
+	return { userData: state.userData };
+};
+
+export default withRouter(connect(mapStateToProps)(App));
