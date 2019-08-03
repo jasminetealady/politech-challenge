928750738f8b9d568aa4d467797a50fe0f7c011a
Razelle McCarrick <razelle@satyaorthopedic.com>
2019-08-03 13:38:55 -0400

    added loading indicator gif and fixed code rendering it

--- src/components/GIF.js
+++ src/components/GIF.js
@@ -4,18 +4,32 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { deleteGif } from '../actions/userDataActions.js';
+import loadingIndicator from '../images/loading.gif';
 
 function GIF({ name, url, loading, icon, deleteGif }) {
 	const handleClick = () => {
 		let currentGif = { url: url };
 		deleteGif(currentGif);
 	};
+
+	const showLoadingOrImage = () => {
+		if (loading) {
+			return <img src={loadingIndicator} alt="loading" />;
+		} else if (!loading && !url) {
+			return null;
+		} else
+			return (
+				<>
+					<p className="GIF-Name">{name}</p>
+					<img src={url} alt="giphy gif" />
+				</>
+			);
+	};
+
 	return (
 		<div className="GIF">
 			{icon && <FontAwesomeIcon onClick={handleClick} icon={faTimes} />}
-			{name && <p className="GIF-Name">{name}</p>}
-			{loading && 'Loading'}
-			{url && <img src={url} alt="giphy gif" />}
+			{showLoadingOrImage()}
 		</div>
 	);
 }
